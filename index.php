0ab90f6ce333f17498fe3be7b05319fb3c1f9be7
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2024-11-28 14:12:37 +0100

    Log in modal + ny side (ikke funktionel)

--- index.php
+++ index.php
@@ -4,22 +4,24 @@ require "settings/init.php";
 <!DOCTYPE html>
 <html lang="da">
 <head>
-	<meta charset="utf-8">
-	
-	<title>Forside</title>
-	
-	<meta name="robots" content="All">
-	<meta name="author" content="Udgiver">
-	<meta name="copyright" content="Information om copyright">
-	
-	<link href="css/styles.css" rel="stylesheet" type="text/css">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+
+    <title>Forside</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
-<div class="container-fluid vh-100">
-    <div class="row g-2 d-flex justify-content-center text-center align-items-center h-100">
+<div class="d-flex align-items-center justify-content-center"
+     style="background-image: url('img/artboard.webp'); background-size: cover; background-position: center; height: 100vh;">
+    <div class="container-fluid vh-100">
+        <div class="row g-2 d-flex justify-content-center text-center align-items-center h-100">
             <div class="col-8 col-md-4 col-xl-3">
                 <h1 class="mb-3">Klima Match!</h1>
                 <h2 class="mb-5">Tryk på START for at starte spillet</h2>
@@ -27,12 +29,45 @@ require "settings/init.php";
                 <div class="row my-5"></div>
                 <div class="row my-5"></div>
                 <div class="row my-5"></div>
-                <button type="button" class="btn btn-success mt-5">START</button>
+                    <button type="button" class="btn btn-success mt-5 " data-bs-toggle="modal" data-bs-target="#exampleModal">START</button>
             </div>
+        </div>
     </div>
 </div>
 
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Log In</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <label class="form-label d-block" for="username">Username</label>
+                <div class="input-group mb-5">
+                    <input class="form-control" id="username" placeholder="">
+                </div>
+                <label class="form-label d-block" for="password">Password</label>
+                <div class="input-group">
+                    <input class="form-control" id="password" placeholder="">
+                </div>
+            </div>
+            <div class="modal-footer d-flex justify-content-between">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Log in</button>
+            </div>
+        </div>
+    </div>
+</div>
 
+<script>
+    const myModal = document.getElementById('myModal')
+    const myInput = document.getElementById('myInput')
+
+    myModal.addEventListener('shown.bs.modal', () => {
+        myInput.focus()
+    })
+</script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
---
+++ levelSelect.php
@@ -0,0 +1,30 @@
+<?php
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Forside</title>
+	
+	<meta name="robots" content="All">
+	<meta name="author" content="Udgiver">
+	<meta name="copyright" content="Information om copyright">
+	
+	<link href="css/styles.css" rel="stylesheet" type="text/css">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<div class="container-fluid vh-100">
+    <div class="row">
+        <div class="fixed-bottom bg-danger">...</div>
+
+    </div>
+</div>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
